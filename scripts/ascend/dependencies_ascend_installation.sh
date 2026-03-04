0ce260fa17c99f48a87a506cfeae4c9f8dae6079
chenkunjie0506 <chenkunjie1@huawei.com>
2026-03-04 18:05:35 +0800

    fix

--- scripts/ascend/dependencies_ascend_installation.sh
+++ scripts/ascend/dependencies_ascend_installation.sh
@@ -105,7 +105,7 @@ echo -e "system packages installed successfully."
 export CPLUS_INCLUDE_PATH=$(echo $CPLUS_INCLUDE_PATH | tr ':' '\n' | grep -v "/usr/local/Ascend" | paste -sd: -)
 
 # Install yalantinglibs
-clone_repo_if_not_exists "yalantinglibs" "https://gitcode.com/gh_mirrors/ya/yaml-cpp.git"
+clone_repo_if_not_exists "yalantinglibs" "https://gitcode.com/gh_mirrors/ya/yalantinglibs.git"
 cd yalantinglibs || exit
 git checkout 0.5.5
 rm -rf build
